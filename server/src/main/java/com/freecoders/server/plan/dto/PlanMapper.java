d96eb373d5d33728ace2edfa2d4f4919f55cf361
Patryk <patrykjanas27@gmail.com>
2023-01-15 00:58:57 +0100

    add new feature

--- server/src/main/java/com/freecoders/server/plan/dto/PlanMapper.java
+++ server/src/main/java/com/freecoders/server/plan/dto/PlanMapper.java
@@ -1,8 +1,13 @@
 package com.freecoders.server.plan.dto;
 
 import com.freecoders.server.entites.Plan;
+import com.freecoders.server.entites.Task;
+import com.freecoders.server.task.dto.TaskDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper(componentModel = "spring", uses = PlanMapper.class)
 public interface PlanMapper {
@@ -12,5 +17,21 @@ public interface PlanMapper {
     @Mapping(target = "description", source = "description")
     @Mapping(target = "tasksDto", source = "tasks")
     PlanDto mapPlanToPlanDto(Plan plan);
+
+    default List<TaskDto> mapTasksToTasksDto(List<Task> tasks) {
+        List<TaskDto> taskDtos = new ArrayList<>();
+        for (int i = 0; i < tasks.size(); i++) {
+            TaskDto taskDto = new TaskDto();
+            taskDto.setId(tasks.get(i).getId());
+            taskDto.setName(tasks.get(i).getName());
+            taskDto.setDescription(tasks.get(i).getDescription());
+            taskDto.setTaskDateTime(tasks.get(i).getTaskDateTime());
+            taskDto.setPromptContext(tasks.get(i).getPromptContext());
+            taskDto.setQuestion(tasks.get(i).getQuestion());
+            taskDtos.add(taskDto);
+        }
+        return taskDtos;
+    }
+
 
 }
--- server/src/main/java/com/freecoders/server/task/dto/TaskDto.java
+++ server/src/main/java/com/freecoders/server/task/dto/TaskDto.java
@@ -3,10 +3,15 @@ package com.freecoders.server.task.dto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @RequiredArgsConstructor
 public class TaskDto {
-    private Long id;
-    private String name;
-    private String description;
+    private  Long id;
+    private  String name;
+    private  String description;
+    private  LocalDateTime taskDateTime;
+    private  String promptContext;
+    private  String question;
 }
